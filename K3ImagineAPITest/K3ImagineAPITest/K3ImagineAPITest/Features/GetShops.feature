d25a28a31721db6d975e17e912f4af3a897e7c32
Akin Oladeji <lahdayg@yahoo.com>
2021-04-20 15:15:30 +0100

    This is my first commit

---
+++ K3ImagineAPITest/K3ImagineAPITest/K3ImagineAPITest/Features/GetShops.feature
@@ -0,0 +1,9 @@
+﻿Feature: GetShops
+	I will like to validate that the shop Koge exist
+	From the list of all shops
+
+@mytag
+Scenario: Validate Shop Koge Exist 
+	Given I navigate to the website and I send a POST request to get the bearer token
+	When I send a GET request to receive the list of shops 
+	Then I should receive a response that shop Koge exist
---
+++ K3ImagineAPITest/K3ImagineAPITest/K3ImagineAPITest/StepDefinition/GetShopsSteps.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using NUnit.Framework;
+
+namespace K3ImagineAPITest.StepDefinition
+{
+    [Binding]
+    public class GetShopsSteps
+
+    {
+         RestClient client;
+         RestRequest request;
+         IRestResponse response;
+         static dynamic token;
+      
+
+
+       [Given(@"I navigate to the website and I send a POST request to get the bearer token")]
+        public void GivenINavigateToTheWebsiteAndISendAPOSTRequestToGetTheBearerToken()
+        {
+            var client = new RestClient("https://portal.k3imagine.com/gw-portal/token");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("content-type", "application/x-www-form-urlencoded");
+            request.AddParameter("application/x-www-form-urlencoded", "grant_type=password&username=akin&password=PasswordSecure44@", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            string content = Regex.Unescape(response.Content);
+            content = content.Substring(1, content.Length - 2);
+            token = JObject.Parse(content);
+        }
+
+        [When(@"I send a GET request to receive the list of shops")]
+        public void WhenISendAGETRequestToReceiveTheListOfShops()
+        {
+            client = new RestClient("https://portal.k3imagine.com/gw-rbo/api/v1.0/shop?includeInactive=true&includePrimaryGroupName=true");
+            request = new RestRequest(Method.GET);
+            request.AddHeader("authorization", "Bearer " + token.access_token);
+            request.AddHeader("cache-control", "no-cache");
+            response = client.Execute(request);
+
+        }
+
+        [Then(@"I should receive a response that shop Koge exist")]
+        public void ThenIShouldReceiveAResponseThatShopKogeExist()
+        {
+            var dataArray = JArray.Parse(response.Content);
+            //bool findKoge = false;
+            foreach (var jToken in dataArray)
+            {
+                var item = (JObject)jToken;
+                if (item.GetValue("description").ToString() == "Koge")
+                    Assert.That(item.GetValue("description").ToString().Contains("Koge"), Is.True); 
+            }
+        }
+    }
+}
